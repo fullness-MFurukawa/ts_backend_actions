49fd461c9caf18ec7dba7478e61737d801bcf1dc
fullness-MFurukawa <furukawa@fullness.co.jp>
2025-04-14 07:29:17 +0000

    getCategoryById()メソッドのテストドライバ

--- test/application/in/interactor/RegisterProductInteractor.spec.ts
+++ test/application/in/interactor/RegisterProductInteractor.spec.ts
@@ -2,6 +2,7 @@ import { INestApplication } from "@nestjs/common";
 import { Test } from "@nestjs/testing/test";
 import { RegisterProductUsecase } from "@src/application/in/usecase/RegisterProductUsecase";
 import { AppModule } from "@src/AppModule";
+import { NotFoundException } from "@src/shared/exception/NotFoundException";
 
 /**
  * RegisterProductInteractorのテストドライバ
@@ -47,6 +48,23 @@ describe('RegisterProductInteractorの単体テスト', () => {
             expect(categoryNames).toContain('文房具');
             expect(categoryNames).toContain('雑貨');
             expect(categoryNames).toContain('パソコン周辺機器');
+        });
+    });
+    describe('getCategoryById()メソッド', () => {
+        it('存在する商品カテゴリIdの商品カテゴリを取得できる', async () => {
+            // 商品カテゴリを取得する
+            const category = await usecase
+                .getCategoryById('b1524011-b6af-417e-8bf2-f449dd58b5c0');
+            // 取得した結果の検証
+            expect(category.id).toBe('b1524011-b6af-417e-8bf2-f449dd58b5c0');
+            expect(category.name).toBe('文房具');
+        });
+        it('商品カテゴリが存在しない場合、NotFoundExceptionがスローされる', async () => {
+            const id = 'b1524011-b6af-417e-8bf2-f449dd58b5c1';
+            // 結果の検証
+            await expect(usecase.getCategoryById(id)).rejects.toThrow(NotFoundException);
+            await expect(usecase.getCategoryById(id)).rejects.toThrow(
+            `商品カテゴリId:(b1524011-b6af-417e-8bf2-f449dd58b5c1)の商品カテゴリは存在しません。`);
         });
     });
 });
